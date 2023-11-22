7f89daec9a463d1c0fa569c27f3f157a44d10861
thatguysilva <silva.unb.phys@gmail.com>
2023-11-22 01:06:11 -0300

    update

---
+++ src/app/reservations/reservations.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reservations',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reservations.component.html',
+  styleUrl: './reservations.component.css'
+})
+export class ReservationsComponent {
+
+}
